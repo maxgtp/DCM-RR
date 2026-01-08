2ad60cbfa0d32ba2f89b312db14c49cd0672edd0
maxgtp <54073311+maxgtp@users.noreply.github.com>
2026-01-08 02:37:47 -0300

    Delete jest.config.js

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.test.js', '**/*.test.js'],
-  collectCoverageFrom: [
-    'js/**/*.js',
-    '!js/**/*.test.js'
-  ],
-  setupFiles: ['<rootDir>/jest.setup.js']
-};
